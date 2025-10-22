7834ef5d253e3e55836070d68d1ec476a3251070
Mr-Vivekkumar <vrszonnett@gmail.com>
2025-10-22 22:17:18 +0530

    code optimization

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 
@@ -14,6 +15,10 @@ export const appConfig: ApplicationConfig = {
     }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
